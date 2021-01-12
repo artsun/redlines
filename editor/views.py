69098cabfe4a97ecab31c46dd25123ae7435502a
artsun <parafo@gmail.com>
2021-01-13 00:42:45 +0300

    Update views.py

--- editor/views.py
+++ editor/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User, Group
 import json
 from collections import OrderedDict
 
-from .models import Image, Slide, Slider, Rubric, Article, ArticleToImage, SliderToArticle, ArticleToRubric
+from .models import Image, Slide, Slider, Rubric, Article, ArticleToImage, SliderToArticle, ArticleToRubric, EditBlock
 
 
 class ArtListPage(View):
@@ -67,13 +67,14 @@ class Editor(View):
         article = Article.objects.get(trans_title=trans_title)
         rubric = ArticleToRubric.objects.get(art=article).rubric
         icon = ArticleToImage.objects.get(art=article) if ArticleToImage.objects.filter(art=article).exists() else None
+        print([x.data for x in EditBlock.objects.filter(art=article).order_by('edit_block_num')])
 
-        sliders = [s.slider for s in SliderToArticle.objects.filter(art=article)]
-
-        context = {'sliders': json.dumps([[x.pk, x.compouse_struct()] for x in sliders]),
+        context = {'sliders':  [(sl.pk, json.loads(sl.content)) for sl in Slider.objects.all().order_by('pk')],  # right panel
+                   'sliders_in_content': [[s.edit_block_num, s.slider.pk] for s in SliderToArticle.objects.filter(art=article).order_by('edit_block_num')],
                    'article': article,
                    'rubric': rubric,
-                   'icon': icon}
+                   'icon': icon,
+                   'eblocks': EditBlock.objects.filter(art=article).order_by('edit_block_num'), }
 
         return render(request, 'editpage.html', context)
 
@@ -82,8 +83,11 @@ class Editor(View):
             return redirect('/')
 
         article = Article.objects.get(trans_title=trans_title)
+        if request.POST.get('getContent'):
+            parts = [x.data for x in EditBlock.objects.filter(art=article).order_by('edit_block_num')]
+            return JsonResponse({'parts': json.dumps(parts)})
 
-        if request.POST.get('artUpd'):
+        elif request.POST.get('artUpd'):
             artUpd = json.loads(request.POST.get('artUpd'))
             if len(artUpd) != 2:
                 return JsonResponse({'url': f'/editor/{article.trans_title}'})
@@ -101,65 +105,67 @@ class Editor(View):
                 ai = ArticleToImage()
                 ai.update_with_image(article, request.FILES.get('icon'))
                 ai.save()
-
+        elif None not in [request.POST.get(x) for x in ('parts', 'sliders', 'htmls')]:
+            args = [request.POST.get(x) for x in ('parts', 'sliders', 'htmls', 'artUpdates')]
+            article.full_update(*args)
+            return JsonResponse({'url': f'/news/{article.trans_title}'})
         return JsonResponse({'url': f'/editor/{article.trans_title}'})
 
 
 class SliderMake(View):
     #login_url = '/login/'
 
-    def get(self, request, trans_title=None, spk=None):
-        print('GET PK', spk)
+    def get(self, request):
+        print('GET PK', request.GET.get('article'), request.GET.get('spk'))
+        trans_title, spk = request.GET.get('article'), request.GET.get('spk')
         if not Article.objects.filter(trans_title=trans_title).exists():
             return redirect('/')
         article = Article.objects.get(trans_title=trans_title)
-        article_edit_url = article.get_edit_url()
+        article_edit_url = f"/editor/{trans_title}"
         if spk is None:
             context = {'slides': Slide.objects.all().order_by('pk'),
-                       'article_edit_url': article.get_edit_url()
+                       'article_edit_url': article_edit_url,
                        }
         else:
             if not Slider.objects.filter(pk=spk).exists():
-                return redirect(f'slider')
+                return redirect(f'/editor/{trans_title}/slider')
 
             slider_instance = Slider.objects.get(pk=spk)
             context = {'slides': Slide.objects.all().order_by('pk'),
                        'slider_struct': json.dumps(slider_instance.compouse_struct()),
-                       'article_edit_url': article.get_edit_url(),
+                       'article_edit_url': article_edit_url,
                        }
 
         return render(request, 'sliderpage.html', context)
 
-    def post(self, request, trans_title=None, spk=None):
-        print('POST, PK', spk)
+    def post(self, request):
+        trans_title, spk = request.GET.get('article'), request.GET.get('spk')
         post_heads = request.POST.keys()
         slider_exists = Slider.objects.filter(pk=spk).exists()
-        if not Article.objects.filter(trans_title=trans_title).exists():
+        if not Article.objects.filter(trans_title=trans_title).exists():    # only for url
             return redirect('/')
-        article = Article.objects.get(trans_title=trans_title)
+        article = Article.objects.get(trans_title=trans_title)  # only for url, because edit only from article
 
         if request.POST.get('delSlide'):
             slide = Slide.objects.get(pk=request.POST.get('delSlide'))
             [slide.remove_from_slider(sl) for sl in set(Slider.objects.filter(slider_slides__slde=slide).all())]
             slide.delete()
-            return redirect(f'/editor/{trans_title}/slider/{spk}') if Slider.objects.filter(pk=spk).exists() else redirect('slider')
+            return redirect(f'/slider?article={trans_title}') if spk is None else redirect(f'/slider?article={trans_title}&spk={spk}')
 
-        elif request.POST.get("updSlider"):
+        elif request.POST.get("updSlider") and request.POST.get("sliderContent"):
             struct = json.loads(request.POST.get("updSlider"), object_pairs_hook=OrderedDict)
             if slider_exists:
                 slider = Slider.objects.get(pk=spk)
                 if len(struct) < 1:
                     slider.delete()
-                    return JsonResponse({'url': f'/editor/{trans_title}/slider'})
+                    return JsonResponse({'url': f'/slider?article={trans_title}'})
             else:
                 if len(struct) < 1:
-                    return JsonResponse({'url': f'/editor/{trans_title}/slider'})
+                    return JsonResponse({'url': f'/slider?article={trans_title}'})
                 slider = Slider()
-                slider.save()
-                sta = SliderToArticle(slider=slider, art=article)
-                sta.save()
-            slider.from_struct(struct)
-            return JsonResponse({'url': f'/editor/{trans_title}/slider/{slider.pk}'})
+                slider.save()  # not force update
+            slider.from_struct_and_content(struct, request.POST.get("sliderContent"))
+            return JsonResponse({'url': f'/slider?article={trans_title}&spk={slider.pk}'})
 
         elif request.FILES.get('photo') and request.POST.get('name'):
             Slide.objects.get(pk=request.POST.get('name')).img.commit(request.FILES.get('photo'), force_update=True)
@@ -168,11 +174,11 @@ class SliderMake(View):
             Slide.objects.get(pk=request.POST.get('name')).upd(request.POST.get('class'), request.POST.get('val'))
 
         elif request.FILES.get('file'):
-            img = Image()
-            img.commit(request.FILES.get('file'))
-            Slide(img=img, label=request.POST.get('label'), descr=request.POST.get('descr')).save()
+            print(request.FILES.get('file'))
+            sl = Slide(img=request.FILES.get('file'), label=request.POST.get('label'), descr=request.POST.get('descr'))
+            sl.save()
 
-        return redirect(f'/editor/{trans_title}/slider') if spk is None else redirect(f'/editor/{trans_title}/slider/{spk}')
+        return redirect(f'/slider?article={trans_title}') if spk is None else redirect(f'/slider?article={trans_title}&spk={spk}')
 
 
 class Uploader(View):

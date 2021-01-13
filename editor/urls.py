8a4b3cceddb72ddf73c96b667b31dc42e7199c6e
artsun <parafo@gmail.com>
2021-01-14 01:15:49 +0300

    Update urls.py

--- editor/urls.py
+++ editor/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArtListPage.as_view()),
-    path('newarticle', views.RubricPage.as_view()),
-    path('editor/<str:trans_title>', views.Editor.as_view()),
-    path('uploadFile', views.Uploader.as_view()),
+    path('', views.ArtListPage.as_view()),  # /editor/
+    path('new', views.RubricPage.as_view()),
+    path('icon', views.IconMake.as_view()),
     path('slider', views.SliderMake.as_view()),
-    #path('editor/<str:trans_title>/slider', views.SliderMake.as_view()),
+    path('uploadFile', views.Uploader.as_view()),
+    path('<str:trans_title>', views.Editor.as_view()),    
 ]

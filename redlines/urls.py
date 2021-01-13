53f3c7c549b590a7886b98592544c07bf0d8d9ee
artsun <parafo@gmail.com>
2021-01-14 01:16:56 +0300

    Create urls.py

---
+++ redlines/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('editor', include('editor.urls')),
+    path('editor/', include('editor.urls')),
+    path('', include('news.urls')),
+    path('news', include('news.urls')),
+    path('news/', include('news.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

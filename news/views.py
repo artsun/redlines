d320856d7660cac0c675275a0abc46d53bb5e0c7
artsun <parafo@gmail.com>
2021-02-13 22:00:41 +0300

    Update views.py

--- news/views.py
+++ news/views.py
@@ -12,20 +12,33 @@ import json
 from collections import OrderedDict
 
 from editor.models import Image, Slide, Slider, Rubric, Article, SliderToArticle, ArticleToRubric
+from management.models import IndexArticleFresh, IndexArticleHot
 
 
 class IndexPage(View):
 
     def get(self, request, trans_title=None):
-        artlist, temp = [], []
-        for x in Article.objects.all().order_by('updated'):
+
+        flist, hlist, pklist, nlist = [], [], [], []
+        for x in IndexArticleFresh.objects.all().order_by('position'):
+            flist.append(x)
+            pklist.append(x.article.pk)
+        for x in IndexArticleHot.objects.all().order_by('position'):
+            hlist.append(x)
+            pklist.append(x.article.pk)
+        temp = []
+        for x in Article.objects.all().exclude(pk__in=pklist).order_by('updated')[:10]:
             temp.append(x)
-            if len(temp) == 5:
-                artlist.append(temp)
+            if len(temp) == 2:
+                nlist.append(temp)
                 temp = []
-                
-        #print(artlist)
-        context = {'artlist': artlist}
+        context = {
+            'flist': flist,
+            'hlist': hlist,
+            'nlist': nlist,
+            'statistics': Rubric.statistics(),
+        }
+        context.update(Rubric.navbar_catalogs())
         return render(request, 'indexpage.html', context)
 
 
@@ -35,6 +48,23 @@ class NewsPage(View):
         if not Article.objects.filter(trans_title=trans_title):
             return redirect('/')
         article = Article.objects.get(trans_title=trans_title)
+        rubric = article.rub.first().rubric
+        prev = Article.objects.filter(rub__rubric=rubric, updated__lt=article.updated).first()
+        next = Article.objects.filter(rub__rubric=rubric, updated__gt=article.updated).first()
+        you_may_also_like = [a.article for a in IndexArticleHot.objects.exclude(article=article) if a.article != prev and a.article != next]
+        also_like_one = you_may_also_like[0] if len(you_may_also_like) > 0 else []
+        also_like_two = you_may_also_like[1] if len(you_may_also_like) > 1 else []
+        print(article.comments.count())
 
-        context = {'article': article}
+        context = {
+            'article': article,
+            'similar': Article.objects.filter(rub__rubric=rubric).exclude(pk=article.pk).order_by('updated')[:5],
+            'news': Article.objects.exclude(rub__rubric=rubric)[:5],
+            'statistics': Rubric.statistics(),
+            'prev': prev,
+            'next': next,
+            'also_like_one': also_like_one,
+            'also_like_two': also_like_two
+        }
+        context.update(Rubric.navbar_catalogs())
         return render(request, 'newspage.html', context)
